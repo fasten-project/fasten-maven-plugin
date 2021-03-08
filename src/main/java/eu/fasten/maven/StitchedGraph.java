3d7c747e5f3f648e31c5f40d9089997d27aa1db9
Thomas Mortagne <thomas.mortagne@gmail.com>
2021-03-08 12:40:09 +0100

    Fixed #3 StackOverflowError when running on some example projects

--- src/main/java/eu/fasten/maven/StitchedGraph.java
+++ src/main/java/eu/fasten/maven/StitchedGraph.java
@@ -24,6 +24,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.collections4.iterators.IteratorChain;
+import org.apache.commons.collections4.iterators.IteratorIterable;
+import org.jgrapht.traverse.DepthFirstIterator;
+
 import eu.fasten.core.data.ArrayImmutableDirectedGraph;
 import eu.fasten.core.data.DirectedGraph;
 import eu.fasten.core.data.ExtendedRevisionJavaCallGraph;
@@ -31,6 +35,8 @@ import eu.fasten.core.data.FastenURI;
 import eu.fasten.core.data.JavaNode;
 import eu.fasten.core.data.JavaScope;
 import eu.fasten.core.data.JavaType;
+import it.unimi.dsi.fastutil.longs.LongLongPair;
+import it.unimi.dsi.fastutil.longs.LongSet;
 
 /**
  * Create and navigate a stitched call graph.
@@ -80,35 +86,46 @@ public class StitchedGraph
 
         Set<Long> handledNodes = new HashSet<>();
 
-        for (final List<Integer> l : projectRCG.getGraph().getGraph().getInternalCalls().keySet()) {
-            appendNodeAndSuccessors(l.get(0).longValue(), stichedBuilder, handledNodes);
-        }
-        for (final List<Integer> l : projectRCG.getGraph().getGraph().getExternalCalls().keySet()) {
-            appendNodeAndSuccessors(l.get(0).longValue(), stichedBuilder, handledNodes);
-        }
+        LongSet externalCalls = this.fullGraph.externalNodes();
+
+        IteratorChain<Long> projectCalls = new IteratorChain<>(
+            projectRCG.getGraph().getGraph().getInternalCalls().keySet().stream().map(l -> l.get(0).longValue())
+                .iterator(),
+            projectRCG.getGraph().getGraph().getExternalCalls().keySet().stream()
+                .map(l -> this.globalIdToGraphId.get(l.get(0).longValue())).iterator());
+
+        appendNodeAndSuccessors(new IteratorIterable<>(projectCalls), stichedBuilder, externalCalls, handledNodes);
 
         this.stichedGraph = stichedBuilder.build();
     }
 
-    private void appendNodeAndSuccessors(long node, ArrayImmutableDirectedGraph.Builder stichedBuilder,
-        Set<Long> handledNodes)
+    private void appendNodeAndSuccessors(Iterable<Long> startVertices,
+        ArrayImmutableDirectedGraph.Builder stichedBuilder, LongSet externalCalls, Set<Long> addedNodes)
     {
-        if (!handledNodes.contains(node)) {
-            addNode(node, stichedBuilder);
+        DepthFirstIterator<Long, LongLongPair> iterator = new DepthFirstIterator<>(this.fullGraph, startVertices);
 
-            handledNodes.add(node);
+        while (iterator.hasNext()) {
+            long edge = iterator.next();
 
-            for (Long successor : this.fullGraph.successors(node)) {
-                appendNodeAndSuccessors(successor, stichedBuilder, handledNodes);
+            if (!addedNodes.contains(edge)) {
+                addNode(edge, stichedBuilder, externalCalls);
+                addedNodes.add(edge);
+            }
 
-                stichedBuilder.addArc(node, successor);
+            for (Long successor : this.fullGraph.successors(edge)) {
+                if (!addedNodes.contains(successor)) {
+                    addNode(successor, stichedBuilder, externalCalls);
+                    addedNodes.add(successor);
+                }
+
+                stichedBuilder.addArc(edge, successor);
             }
         }
     }
 
-    private void addNode(long node, ArrayImmutableDirectedGraph.Builder stichedBuilder)
+    private void addNode(long node, ArrayImmutableDirectedGraph.Builder stichedBuilder, LongSet externalCalls)
     {
-        if (this.fullGraph.externalNodes().contains(node)) {
+        if (externalCalls.contains(node)) {
             stichedBuilder.addExternalNode(node);
         } else {
             stichedBuilder.addInternalNode(node);
